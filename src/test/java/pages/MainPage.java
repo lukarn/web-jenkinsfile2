fd8d4ba0a2854994089b6454d23ea3347670bd1a
Łukasz Arndt <lukarn@gmail.com>
2020-02-22 20:16:01 +0100

    edit inspection profile settings

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -3,7 +3,7 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+//import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class MainPage extends Page {
 
